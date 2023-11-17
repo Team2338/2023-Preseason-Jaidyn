f5b6716de84166fcfa9e6b403a1929df813f43dc
Tharun S <30896231@students.sd308.org>
2023-11-16 18:48:02 -0600

    testing needed.

--- src/main/java/team/gif/robot/RobotMap.java
+++ src/main/java/team/gif/robot/RobotMap.java
@@ -1,8 +1,15 @@
 package team.gif.robot;
 
+
 public abstract class RobotMap {
     // Controllers
     public static final int DRIVER_CONTROLLER_ID = 0;
     public static final int AUX_CONTROLLER_ID = 1;
     public static final int TEST_CONTROLLER_ID = 2;
+
+    //drivetrain
+    public static final int LEFT_MOTOR = 0;
+    public static final int RIGHT_MOTOR = 1;
+
+    public static final int PIGEON = 12;
 }
